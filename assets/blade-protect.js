1ff9e06145bc5232d8b4775e6659e335b462462a
Mark Beech <mbeech@mark-beech.co.uk>
2019-03-03 20:07:22 +0000

    Rename files

---
+++ assets/blade-protect.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var protections = document.querySelectorAll('.__blade_protect');
+    if (protections.length > 0) {
+        protections.forEach((protect) => {
+            var protect = protect;
+
+            send(protect.value)
+            setInterval(send(protect.value), 5000);
+
+        });
+    }
+});
+
+function send(value) {
+    fetch("/__blade-protect", {
+        method: "POST", 
+        body: JSON.stringify(value),
+        headers: {
+            'X-CSRF-TOKEN': document.head.querySelector("[name=csrf-token]").content,
+        },
+    })
+}
